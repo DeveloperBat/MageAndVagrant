6d9f8b7d66d463826afb8a58e8d62c751e4d0b36
DevBat <30550431+DeveloperBat@users.noreply.github.com>
2018-03-07 16:06:29 +0100

    Yes
    Yes

---
+++ Assets/Scripts/New Better Scripts/SceneTransition.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider2D))]
+public class SceneTransition : MonoBehaviour {
+
+    public int sceneIndex;
+
+    private Collider2D _collider;
+    private int triggerCount;
+
+    private void Start()
+    {
+        _collider = GetComponent<Collider2D>();
+        _collider.isTrigger = true;
+    }
+
+    private void Update()
+    {
+        if(triggerCount == 2)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            triggerCount += 1;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            triggerCount -= 1;
+        }
+    }
+
+}
