eeb7c29edd6757c07733bdaffd285637cf3e15c9
slypez <slypez@hotmail.com>
2018-03-08 13:06:09 +0100

    Sköldfix
    saker ;S

--- Assets/Scripts/RotateControllers.cs
+++ Assets/Scripts/RotateControllers.cs
@@ -38,7 +38,7 @@ public class RotateControllers : RotateToJoystick{
 
             difference.Normalize();
             rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y, rotation_z - 90));
+            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y, rotation_z + 90));
         }
         else
         {
