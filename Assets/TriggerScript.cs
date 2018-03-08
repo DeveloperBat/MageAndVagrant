6586b978c1b73a2adbfe422dc42b7d980c9abbf3
DevBat <30550431+DeveloperBat@users.noreply.github.com>
2018-03-08 18:00:12 +0100

    Yap

--- Assets/TriggerScript.cs
+++ Assets/TriggerScript.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class TriggerScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    ds = 
+
+    void OnTriggerEnter2D()
+    {
+        
+    }
+
 }
