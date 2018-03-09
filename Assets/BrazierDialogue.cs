e8093de00ce19fe1a7774c66b47047e9d08686e4
DevBat <30550431+DeveloperBat@users.noreply.github.com>
2018-03-09 11:37:14 +0100

    Commit

---
+++ Assets/BrazierDialogue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrazierDialogue : MonoBehaviour {
+
+    Brazier brazier;
+    DialogueSystem ds;
+
+    public string dialogue;
+    bool activated = false;
+
+    void Start()
+    {
+        brazier = GetComponent<Brazier>();
+        ds = GameObject.Find("Dialogue Canvas").GetComponent<DialogueSystem>();
+    }
+
+    void Update()
+    {
+        if (brazier.isBurning && activated == false)
+        {
+            activated = true;
+            ds.PrintDialogue(dialogue);
+        }
+    }
+
+}
